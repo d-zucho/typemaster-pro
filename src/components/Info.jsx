cb41d4f21cfa07733de2133427fd1463d7c32626
D_Zucho <danny.nico.dev@outlook.com>
2024-01-13 07:29:27 -0800

    finish v1 of info section

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -3,8 +3,11 @@ import KeyboardAndGlass from '../assets/desktop/image-glass-and-keyboard.jpg'
 
 const Info = () => {
   return (
-    <section id='info-section' className='grid grid-cols-3 gap-5 mt-20'>
-      <div id='img1' className='relative bg-primary w-1/2 rounded-[20px]'>
+    <section
+      id='info-section'
+      className='grid grid-cols-12 gap-6 my-20 h-[500px]'
+    >
+      <div id='img1' className='relative bg-primary col-span-3 rounded-[20px]'>
         {/* <div id='imgOverlay'></div> */}
         <img
           src={KbAndPhone}
@@ -12,13 +15,23 @@ const Info = () => {
           className='w-full h-full absolute object-cover mix-blend-overlay rounded-2xl'
         />
       </div>
-      <div id='img2'>
-        <img src={KeyboardAndGlass} alt='Keyboard' />
+      <div id='img2' className='rounded-2xl col-span-5 max-w-[450px]'>
+        <img
+          src={KeyboardAndGlass}
+          alt='Keyboard'
+          className='rounded-2xl object-cover w-full h-full'
+        />
       </div>
-      <div id='img2'>
-        <img src={KeyboardAndGlass} alt='Keyboard' />
+      <div
+        id='img2'
+        className='flex flex-col text-3xl font-bold text-secondary justify-center items-center col-span-3'
+      >
+        <h2>
+          <span className='block leading-9'>Mechanical</span>{' '}
+          <span className='block leading-9'>Wireless</span>{' '}
+          <span className='block leading-9'>Keyboard</span>
+        </h2>
       </div>
-      {/* <h2>Mechanical Wireless Keyboard</h2> */}
     </section>
   )
 }
